483e6008376e4a230dcdc3e1222d0c42374e0001
Bryan Richter <b@chreekat.net>
2014-11-04 13:13:35 +0100

    Add insertAfter and delete functionality

--- spec/boxlistSpec.js
+++ spec/boxlistSpec.js
@@ -7,7 +7,28 @@ describe("Box", function () {
             a.insertAfter(b);
             a.insertAfter(c);
             expect(a.next).toBe(c);
+            expect(c.prev).toBe(a);
             expect(c.next).toBe(b);
+            expect(b.prev).toBe(c);
         });
     });
-});
+
+    describe("delete", function () {
+        it("patches up the list", function () {
+            a = new Box(3);
+            b = new Box(4);
+            c = new Box(5);
+            a.insertAfter(b);
+            a.insertAfter(c);
+            c.delete();
+            expect(a.next).toBe(b);
+            expect(b.prev).toBe(a);
+        });
+    });
+
+    describe("fromList", function () {
+        it("makes a linked list");
+        it("...with back links");
+    });
+
+})
--- src/boxlist.js
+++ src/boxlist.js
@@ -5,7 +5,18 @@ Box = function (id) {
 
 Box.prototype.insertAfter = function (newBox) {
     if (this.next !== null) {
+        this.next.prev = newBox;
         newBox.next = this.next;
     }
     this.next = newBox;
+    newBox.prev = this;
+};
+
+Box.prototype.delete = function () {
+    if (this.prev !== null) {
+        this.prev.next = this.next;
+    }
+    if (this.next !== null) {
+        this.next.prev = this.prev;
+    }
 };
